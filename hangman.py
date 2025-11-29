7d555811f49dcd7c028fac27b8034f635fc5f169
yonatanRec <yonatanrechnitz@gmail.com>
2025-11-29 18:30:03 +0200

    Refactor hangman.py to implement guessing logic

--- hangman.py
+++ hangman.py
@@ -1,2 +1,15 @@
-word = """ idk just temporary """
-# sasdasd
+word = "string"
+
+def hide():
+    return '_' * len(word)
+
+curr = hide()
+
+n = len(word)
+
+def guess(letter, curr):
+    letter = letter.lower()
+    return ''.join(
+        word[i] if word[i].lower() == letter else curr[i]
+        for i in range(n)
+    )
