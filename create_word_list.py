658d1422e185ce8dc21529a2bed0f15886dec39c
pro-hacker-noob <eran.feldman3@gmail.com>
2025-11-30 07:59:54 +0200

    Refactor word list handling to filter and save selected words to a new file

--- create_word_list.py
+++ create_word_list.py
@@ -2,12 +2,11 @@ import random
 
 try:
     file_path = "top_english_nouns_lower_10000.txt"
-
+    output_path = "filtered_word_list.txt"
     with open(file_path, "r") as file:
         content = file.read()
         word_list = content.splitlines()
         chosen_word = word_list[random.randint(0, len(word_list) - 1)]
-    print(chosen_word)
 
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
