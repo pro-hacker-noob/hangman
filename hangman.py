02ace144b671817539a0d45495f181bd40d80a14
yonatanRec <yonatanrechnitz@gmail.com>
2025-11-30 08:47:59 +0200

    Refactor hide function and enhance guess feedback
    Refactored code and enhanced guess efficiency

--- hangman.py
+++ hangman.py
@@ -1,23 +1,21 @@
 word = "string"
-word_len = len(word)
 
+def hide(word):
+    return ["_"] * len(word)
 
-def hide(word=word):
-    return "_" * len(word)
-
-
-curr = hide()
-
+curr = hide(word)
 
 def guess(letter, curr, word):
     letter = letter.lower()
     word = word.lower()
-    if letter not in word:
-        return False
-    result = curr[:]
+
+    result = curr[:]  # list copy
+    found = False
 
     for i, ch in enumerate(word):
         if ch == letter:
             result[i] = letter
+            found = True
 
-    return result
+    return result, found
+
