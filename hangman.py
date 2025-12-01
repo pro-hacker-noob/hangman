259efae915365b00426a046dac22bed2f9dc258e
pro-hacker-noob <eran.feldman3@gmail.com>
2025-12-01 07:38:33 +0200

    added fallback for hangman.py

--- hangman.py
+++ hangman.py
@@ -11,6 +11,9 @@ try:
 except FileNotFoundError:
     print(f"Error: The file '{file_path}' was not found.")
     word = "ERROR"
+except Exception as e:
+    print(f"An error occurred: {e}")
+    word = "ERROR"
 
 
 def hide(word):
